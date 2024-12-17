149889e5345f4e2a200ba5f36e40a6f9fbd623c0
Abdulbasir456 <abdulbasirhasan@gmail.com>
2024-12-17 13:15:29 +0000

    The bug in the user registration functionality has been fixed.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,16 +6,21 @@ const registerUser = async (req, res) => {
 
 
     try {
-        const userExists = User.findOne({email});
+        const userExists = await User.findOne({email});
 
-        if (userExists) return res.status(400).json({ message: 'User Already Exists' });
+        if (userExists) {
+            return res.status(400).json({ message: 'User Already Exists' });
+        }
 
+        // Create a new user
         const user = await User.create({ name, email, password });
         
+        // return success message
         res.status(201).json({ id: user._id, name: user.name, email: user.email });
 
     } catch (error) {
-        res.status(500).json({message: 'Server error', error });
+        console.error('Error in registerUser:', error.message);
+        res.status(500).json({message: 'Server error', error: error.message });
     }
 };
 
