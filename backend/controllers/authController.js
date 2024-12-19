3dc6d40e3097efb2942bfb2014901d14e412efd0
Abdulbasir456 <abdulbasirhasan@gmail.com>
2024-12-19 17:14:48 +0000

    The password, which was previously stored in plain text, is now hashed  in the database.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 //User Registration 
 const registerUser = async (req, res) => {
@@ -16,13 +17,19 @@ const registerUser = async (req, res) => {
         const user = await User.create({ name, email, password });
         
         // return success message
-        res.status(201).json({ id: user._id, name: user.name, email: user.email });
+        res.status(201).json({ 
+            id: user._id, 
+            name: user.name, 
+            email: user.email, 
+            token: generateToken(user._id),
+        });
 
     } catch (error) {
         console.error('Error in registerUser:', error.message);
         res.status(500).json({message: 'Server error', error: error.message });
     }
 };
+
 
 
 // User Login
@@ -35,25 +42,40 @@ const loginUser = async (req, res) => {
 
         const user = await User.findOne({email});
 
-        if(!user) { 
+        if(!user || !(await user.matchPassword(password))) { 
             console.log('User not found');
             return res.status(401).json({message: 'Invalid Credentials' });
         }
 
-
+        /*
         // Compare passwords
         if (user.password !== password) {
             console.log('Password does not match');
             return res.status(401).json({message: 'Invalid Credentials '});
         }
 
+        */
+
+
         console.log('Login successful');
-        res.json({id: user._id, name: user.name, email: user.email });
+        res.json({
+            id: user._id, 
+            name: user.name, 
+            email: user.email,
+            token: generateToken(user._id),
+         });
 
     } catch (error) {
         console.error('Error in loginUser:', error.message);
         res.status(500).json({message: 'Server error', error: error.message });    
     }
+};
+
+
+// Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+
 };
 
 module.exports = {registerUser, loginUser};
